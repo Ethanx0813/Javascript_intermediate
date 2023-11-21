320f9872ddb525d6ce06d1c0cded401dedb08059
Ankit Yadav <100750820+Ethanx0813@users.noreply.github.com>
2023-11-21 12:45:38 +0530

    Update closures_iffe.js

--- closures_iffe.js
+++ closures_iffe.js
@@ -4,3 +4,12 @@
     const sum = num1 + num2;
     console.log(`Sum of ${num1} and ${num2} is: ${sum}`);
 })();
+function calculateTax(taxRate) {
+    return function(purchaseAmount) {
+        const taxAmount = purchaseAmount * (taxRate / 100);
+        console.log(`Tax amount for a purchase of $${purchaseAmount} at ${taxRate}% tax rate: $${taxAmount.toFixed(2)}`);
+        return taxAmount;
+    };
+}
+const calculateVAT = calculateTax(20); 
+const vatAmount = calculateVAT(100); 
